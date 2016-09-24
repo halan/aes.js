0cdff9996112f76cbcbf7417ded8d03033f3c200
Halan Pinheiro <halan.pinheiro@gmail.com>
2016-09-24 19:52:36 -0300

    Refactoring expandKey to remove 'for' and 'let' to use reducers instead

--- src/expandKey.js
+++ src/expandKey.js
@@ -1,34 +1,36 @@
 import subBytes from './steps/subBytes'
 import { xor, group } from './utils'
 
-const rcon = [0x8d,0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80,0x1b,0x36,0x6c,0xd8,0xab,0x4d,0x9a]
+const RCON = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36]
+
+const WORDS_IN_A_KEY = 4
 
 const WORD_SIZE = 4
-const ROUNDS = 10
 
 const rotWord = word => [...word.slice(-(WORD_SIZE-1)), word[0]]
 
 const subWord = subBytes
 
-const xorRcon = (word, i) => [ rcon[i] ^ word[0], ...word.slice(1) ]
+const xorFirstByte = (word, value) => [ value ^ word[0], ...word.slice(1) ]
 
-const keySchedule = (word, i) => xorRcon(subWord(rotWord(word)), i)
+const keyScheduleCore = (word, rcon) => xorFirstByte(subWord(rotWord(word)), rcon)
 
-const lastWords = (word, behind) => word.slice(-behind*WORD_SIZE).slice(0, WORD_SIZE)
+const lastWord = (arr) => arr.slice(-WORD_SIZE)
 
-// # IMPROVEME
-// 
-// I want to avoid the `let` and the `for` here
-// 
-export default key => {
-  let tmp, words = [...key]
+const generate = (key, rcon) => {
+  const base = keyScheduleCore(lastWord(key), rcon)
+  const words = group(key, WORDS_IN_A_KEY)
 
-  for(let i = WORD_SIZE; i < WORD_SIZE*(ROUNDS+1); i++) {
-    tmp = lastWords(words, 1)
-    if(i % WORD_SIZE === 0) tmp = keySchedule(tmp, i/WORD_SIZE)
-
-    words = [...words, ...xor(lastWords(words, WORD_SIZE), tmp)]
-  }
-  
-  return group(words, key.length)
+  return words.reduce( (k, word) => (
+    [...k, ...xor(word, k.length ? lastWord(k) : base )]
+  ), [])
 }
+
+export default key => (
+  RCON.reduce( (keys, rcon) => {
+    const [last] = [...keys].reverse()
+    const current = generate(last, rcon)
+
+    return [...keys, current]
+  }, [key] )
+)
