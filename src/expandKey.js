71fbf6c080766eb64d8e15b237587dcd2415911a
Halan Pinheiro <halan.pinheiro@gmail.com>
2016-09-24 20:23:42 -0300

    Improve expand keys with es6-candies

--- src/expandKey.js
+++ src/expandKey.js
@@ -14,13 +14,6 @@ import { xor, group } from './utils'
 // 1 RCON para cada nova chave criada, ou seja: 10 rcons.
 const RCON = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36]
 
-// Constante para definir o tamanho de uma chave em words:`4`
-// Ou seja, 16 bytes, 16 letras, 128 bits; esse é o tamanho de uma chave.
-// O AES foi construído para trabalhar com chaves de até 256 bits, mas decidi
-// não implementar a encriptação utilizando outros tamanhos de chave. Se fosse o caso,
-// haveria mais rodadas e portanto mais chaves e um rcon maior.
-const WORDS_IN_A_KEY = 4
-
 // Constante pra definir o tamanho em bytes de um word: `4`
 const WORD_SIZE = 4
 
@@ -28,7 +21,7 @@ const WORD_SIZE = 4
 
 // Esta função rotaciona os bytes de um word, ex:
 // `[0, 1, 2, 3] -> [1, 2, 3, 0]`
-const rotWord = word => [...word.slice(-(WORD_SIZE-1)), word[0]]
+const rotWord = ([first, ...rest]) => [...rest, first]
 
 // subWord é a mesma coisa que o subBytes. No caso da expansão de chave esse procedimento é feito
 // sobre uma word. Todo o procedimento de expansão de chave é baseado em words, que funcionam como
@@ -38,7 +31,7 @@ const subWord = subBytes
 
 // Faz um xor no primeiro byte de uma word com rcon especificado.
 // Esse procedimento é feito exatamente 16 vezes, um para cada valor de rcon.
-const xorFirstByte = (word, value) => [ value ^ word[0], ...word.slice(1) ]
+const xorFirstByte = ([first, ...rest], value) => [ value ^ first, ...rest ]
 
 // Procedimento principal da expansão de chave. Aqui aplicamos a sequência:
 // `rotWord`, `subWord` e por fim o `xor` no `rcon` correspondente da rodada.
@@ -46,8 +39,11 @@ const xorFirstByte = (word, value) => [ value ^ word[0], ...word.slice(1) ]
 // é aplicada tantas vezes iguais ao número de rodadas, que no noso caso são 11.
 const keyScheduleCore = (word, rcon) => xorFirstByte(subWord(rotWord(word)), rcon)
 
-// Função auxiliar que devolve as últimas words. A ideia aqui é pegar a primeira word da última chave
-const lastWord = (arr) => arr.slice(-WORD_SIZE)
+// Função auxiliar que devolve as últimas words. A ideia aqui é pegar a primeira word da última chave.
+const lastWord = arr => arr.slice(-WORD_SIZE)
+
+// Função auxiliar que divide um array em words, ou seja, divide em grupos de 4 elementos.
+const splitInWords = arr => group(arr, WORD_SIZE)
 
 // ### Geração das chaves
 
@@ -57,7 +53,7 @@ const generate = (key, rcon) => {
   // Primeiramente calculo o valor base usando KeyScheduleCore que vai receber a última word da chave.
   const base = keyScheduleCore(lastWord(key), rcon)
   // divido a chave em grupos formando 4 words.
-  const words = group(key, WORDS_IN_A_KEY)
+  const words = splitInWords(key)
 
   // Então faço um reduce em cima dessas words, ou seja, um loop de 4 iterações.
   return words.reduce( (k, word) => (
