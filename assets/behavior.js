b3d2a16ee602cc5a65f68d305ffe44bb544e887f
Halan Pinheiro <halan.pinheiro@gmail.com>
2016-09-24 22:29:47 -0300

    undefined

--- assets/behavior.js
+++ assets/behavior.js
@@ -103,14 +103,6 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "type": "heading",
               "data": {
                 "level": 3,
-                "title": "KeyScheduleCore",
-                "slug": "keyschedulecore"
-              },
-              "depth": 3
-            }, {
-              "type": "heading",
-              "data": {
-                "level": 3,
                 "title": "Geração das chaves",
                 "slug": "gerao-das-chaves"
               },
