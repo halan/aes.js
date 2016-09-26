163404525e2162c510f0031ba5f17921db2b1b27
Halan Pinheiro <halan.pinheiro@gmail.com>
2016-09-25 21:43:27 -0300

    undefined

--- assets/behavior.js
+++ assets/behavior.js
@@ -29,6 +29,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "type": "heading",
           "data": {
             "level": 2,
+            "title": "Aviso",
+            "slug": "aviso"
+          },
+          "depth": 2
+        }, {
+          "type": "heading",
+          "data": {
+            "level": 2,
             "title": "Resumo",
             "slug": "resumo"
           },
